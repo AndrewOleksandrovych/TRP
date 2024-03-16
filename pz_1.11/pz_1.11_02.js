0b7da523cb11a1e73e085283490655eda336526a
AndriiKozhukhar <kozuharandrij@gmail.com>
2024-03-16 11:50:50 +0200

    i`m completed 2 task

--- pz_1.11/pz_1.11_02.js
+++ pz_1.11/pz_1.11_02.js
@@ -1,9 +1,14 @@
 function findUniqueNumber(numbers) {
+  if(numbers.length%2==1){
     let uniqueNumber = numbers[0];
     for (let i = 1; i < numbers.length; i++) {
-      uniqueNumber &= numbers[i];
+      uniqueNumber ^= numbers[i];
     }
     return uniqueNumber;
   }
+  else{
+    console.log("array length is not correct");
+  }
+}
   let arr=[17,18,18,19,15,19,17];
   console.log(findUniqueNumber(arr));
